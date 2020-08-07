e306c77296f142533297f4f096187c1856db0d2c
Freddy Lyle <OdincoGaming@gmail.com>
2020-08-07 15:06:43 -0500

    Update PlayerController.cs

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     public void SetPlayer(GymCard card)
     {
+
         //Ensure players are loaded properly
         if(!this.player1.HasCard())
         {
